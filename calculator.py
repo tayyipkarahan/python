7babb0e06f0d7deb0e28d629ea38ee0e5de38ca4
tayyipkarahan <tayyipkarahan2021@gmail.com>
2022-04-17 00:06:17 +0300

    calculator.py was added

---
+++ calculator.py
@@ -0,0 +1,6 @@
+calculator = {"+" : (lambda x ,y : x + y),
+              "-" : (lambda x ,y : x - y),
+              "*" : (lambda x ,y : x * y),
+              "/" : (lambda x ,y : x / y)}
+
+calculator["+"](6, 4)
